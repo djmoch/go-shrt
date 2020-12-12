591dbe5e6e667388fab993a199b4c41205874099
Daniel Moch <daniel@danielmoch.com>
2020-12-12 11:29:22 -0500

    Add per-file license information where missing

--- doc.go
+++ doc.go
@@ -1,3 +1,5 @@
+// See LICENSE file for copyright and license details
+
 // The shrt module implements a simple (perhaps simplistic) URL
 // shortener. It also handles go-get requests. See the cmd/shrt
 // package documentation for a fuller explanation of the server.
