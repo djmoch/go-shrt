4c260afe18a3e931b9f718d0cca68d20c1dedac4
Daniel Moch <daniel@danielmoch.com>
2020-12-23 07:01:25 -0500

    Bump version

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ MANPATH := ${PREFIX}/man
 
 SRC = cmd/shrt/*.go *.go
 DIST_SRC = cmd *.go Makefile config.mk shrt.1 shrtfile.5 README LICENSE go.mod go.sum
-VERSION := 0.2.2
+VERSION := 0.2.3-dev0
 
 GO_LDFLAGS := "-X main.version=${VERSION}"
 GO := go
