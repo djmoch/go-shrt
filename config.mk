5cfc98db385faa11006962a2a24ca51790dd7743
Daniel Moch <daniel@danielmoch.com>
2020-12-23 07:00:37 -0500

    shrt v0.2.2

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ MANPATH := ${PREFIX}/man
 
 SRC = cmd/shrt/*.go *.go
 DIST_SRC = cmd *.go Makefile config.mk shrt.1 shrtfile.5 README LICENSE go.mod go.sum
-VERSION := 0.2.2-dev0
+VERSION := 0.2.2
 
 GO_LDFLAGS := "-X main.version=${VERSION}"
 GO := go
